94b0e9127909ffe159aa3a2b3253e8aaab860aad
imsp18 <shivangparmar537@gmail.com>
2025-01-05 21:07:08 +0530

    add Hero component with layout structure and image integration

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,21 @@
+import Image from "next/image"
+
 const Hero = () => {
     return (
-        <div>
-            Hero
-        </div>
+        <section className="max-container padding container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500">
+            <div className="hero-map">
+                {/* Left */}
+                <div className="relative z-20 flex flex-1 xl:w-1/2">
+                    <Image
+                        src="/camp.svg"
+                        alt="camp"
+                        width={50}
+                        height={50}
+                        className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
+                    />
+                </div>
+            </div>
+        </section>
     )
 }
 
