9f3d10a90d79f03a014ea4b322082e8444cd1b65
imsp18 <shivangparmar537@gmail.com>
2025-01-09 17:46:43 +0530

    add destination and start track information to Guide component

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -19,7 +19,30 @@ const Guide = () => {
                         Only with the hilink application you'll longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. Invite your friends, relatives to have fun in the wilderness through the valley and reach the top of the mountain.
                     </p>
                 </div>
-
+            </div>
+            <div className="flexCenter max-container relative w-full">
+                <Image
+                    src="/boat.png"
+                    alt="boat"
+                    width={1440}
+                    height={580}
+                    className="w-full object-cover object-center 2xl:rounded-5xl"
+                />
+                <div className="absolute flex bg-white py-8 pl-5 pr-7 rounded-3xl border shadow-md md:left-[5%] left lg:top-20">
+                    <div className="flexBetween flex-col">
+                        <div className="flex w-full flex-col">
+                            <div className="flexBetween w-full">
+                                <p className="regular-16 text-gray-20">Destination</p>
+                                <p className="bold-16 text-gray-50">48 min</p>
+                            </div>
+                            <p className="bold-20 mt-2">Aguas Calientes</p>
+                        </div>
+                        <div className="flex w-full flex-col">
+                            <p className="regular-16 text-gray-20">Start Track</p>
+                            <p className="bold-20 mt-2 whitespace-nowrap">Wonorejo Pasuruan</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
