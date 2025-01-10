f8e7fa14111eba185492b425ccde82b50a9e02f1
imsp18 <shivangparmar537@gmail.com>
2025-01-10 23:01:10 +0530

    add Features component with layout and images for enhanced presentation

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,8 +1,32 @@
+import Image from "next/image"
+
 const Features = () => {
     return (
-        <div>
-            Features
-        </div>
+        <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+            <div className="max-container padding-container relative w-full justify-end">
+                <div className="flex flex-1 lg:min-h-[900px]">
+                    <Image
+                        src="/phone.png"
+                        alt="phone"
+                        width={440}
+                        height={1000}
+                        className="feature-phone"
+                    />
+                </div>
+                <div className="z-20 flex w-full flex-col lg:w-[60%]">
+                    <div className="relative">
+                        <Image
+                            src="/camp.svg"
+                            alt="camp"
+                            width={50}
+                            height={50}
+                            className="absolute left-[-5px] top-[-10px] w-10 lg:w-[50px]"
+                        />
+                        <h2 className="bold-40 lg:bold-64">Our Features</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
